3ee7c1e567fa4d52d401b909c86ce5400b5fc5db
ravi <ravi@dobbyads.com>
2020-08-15 14:48:32 +0530

    more routes added

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ app.get('/', (req, res) => {
     res.send("Basic Express Setup");
 });
 
+app.get('/help', (req, res) => {
+    res.send('Help Route')
+})
+
+app.get('/notFound', (req, res) => {
+    res.send('Not Found Route')
+})
+
+app.get('/login', (req, res) => {
+    res.send('User Login Route')
+})
+
+app.get('/userProfile', (req, res) => {
+    res.send('Profile Route')
+})
+
 app.listen(serverPort);
 console.log('Server is listening at port ' + (serverPort));
